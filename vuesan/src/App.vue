fa9fd094660f015160cff83b8aca94580beb23d0
SEB_Full_36기_서외구 <89363048+sud665@users.noreply.github.com>
2022-05-25 11:31:26 +0900

    [Vue]_cli_설치

---
+++ vuesan/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h4>노형 아파트</h4>
+    <p>{{ price1 }}만원</p>
+    <h4>노형 아파트</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: "App",
+  data() {
+    return {
+      price1: 60,
+      price2: 100,
+    };
+  },
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
